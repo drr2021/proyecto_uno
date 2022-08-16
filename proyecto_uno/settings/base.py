85e6aec5c24ab913353dc9802fc3aab7d49252f1
DarioRR <romero.darior@gmail.com>
2022-08-16 00:15:59 -0300

    otra app llamado blog

--- proyecto_uno/settings/base.py
+++ proyecto_uno/settings/base.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ejemplo.apps.EjemploConfig',
-    'bootstrap4'
+    'bootstrap4',
+    'blogapp'
 ]
 
 MIDDLEWARE = [
